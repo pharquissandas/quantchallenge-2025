4458068ba19bfca4a6682cd2d3b8a51ceb0d7015
Preet Harquissandas <96826908+pharquissandas@users.noreply.github.com>
2025-09-23 00:02:17 +0100

    Functions written for template.py

--- trading/template.py
+++ trading/template.py
@@ -1,11 +1,11 @@
 """
 Quant Challenge 2025
-
 Algorithmic strategy template
 """
-
 from enum import Enum
-from typing import Optional
+from typing import Optional, Dict
+import time
+import math
 
 class Side(Enum):
     BUY = 0
@@ -67,7 +67,7 @@ def cancel_order(ticker: Ticker, order_id: int) -> bool:
     success
         True if order was cancelled, False otherwise
     """
-    return 0
+    return False
 
 class Strategy:
     """Template for a strategy."""
@@ -81,11 +81,66 @@ class Strategy:
         Note: In production execution, the game will start from the beginning
         and will not be replayed.
         """
-        pass
+        self.bestBid: Optional[float] = None
+        self.bestAsk: Optional[float] = None
+        self.midPrice: Optional[float] = None
+
+        self.capitalRemaining: float=100000.0
+        self.position: float = 0.0
+        self.averageEntryPrice: Optional[float] = None
+        self.untappedPNL: float = 0.0
+        self.openLimitOrders: Dict[int,Dict] = {}
+        self.homeScore: int=0
+        self.awayScore: int=0
+        self.timeRemaining: Optional[float] = None
+        self.gameLength: Optional[float] = None
+
+        # Conservative parameters
+
+        self.maxcapitalperSide: float=0.05
+        self.edgeThreshold: float=4.0
+        self.maxEdge: float=12.0
+        self.minTradeIn: float=50.0
+        self.minContractsperTrade: int=1
+        self.maxContractsperTrade: int=500
+        self.tickSize: float=0.1
+        self.cancelTimeLimitOrder: float=5.0
+        self.checkStaleOrders: float=1.0
+        self.lastCancelCheck: float=time.time()
+
+        self.exitPosition: float=3.0
+        self.maxTradesperDay: int=200
+
+        self.tradeNo: int=0
+        self.logPrefix = "[Conservative]"
+        self.logText("Started conservative strategy")
 
     def __init__(self) -> None:
         """Your initialization code goes here."""
         self.reset_state()
+
+    def logText(self, message:str) -> None:
+        print(f"{self.logPrefix} {message}")
+
+    def updateMidprice(self) -> None:
+        if (self.bestBid is not None) and (self.bestAsk is not None):
+            self.midPrice = max(0.0, min(100.0, (self.bestBid+self.bestAsk)/2.0))
+        elif self.bestBid is not None:
+            self.midPrice = self.bestBid
+        elif self.bestAsk is not None:
+            self.midPrice = self.bestAsk
+        else:
+            self.midPrice=None 
+
+    def calculateWinProb(self, homeScore: int, awayScore: int, time: Optional[float]) -> float:
+        
+        # finds difference and remaining time
+
+        return probability
+    
+    def allowedContracts(self, price: float) -> float:
+
+        return ______
 
     def on_trade_update(
         self, ticker: Ticker, side: Side, quantity: float, price: float
